f719364338721e16823eaedd510b7bd861f0633b
Natanael dos Santos Feitosa <52074821+natanfeitosa@users.noreply.github.com>
2021-08-31 10:20:16 +0000

    bugs home controller correction

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const app = express(),
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/v1', cors, routes)
+app.use('/v1', routes)
 
 swagger_doc['info']['version'] = pkg['version']
 
 if (process.env.NODE_ENV == 'development'){
-    swagger_doc['host'] = 'https://animes-api.natanapps.repl.co'
+    swagger_doc['host'] = 'animes-api.natanapps.repl.co'
 }
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swagger_doc))
