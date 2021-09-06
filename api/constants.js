50aa1d151079bb28426243bf90dc67936e069836
Natanael dos Santos Feitosa <52074821+natanfeitosa@users.noreply.github.com>
2021-09-05 21:27:30 -0300

    base_api updated for dev mode or prod mode

--- api/constants.js
+++ api/constants.js
@@ -1,3 +1,3 @@
 exports.base_url = 'https://www.anitube.site/'
 
-exports.base_api = 'https://animes.natanapps.repl.co/v1/'
+exports.base_api = proccess.env.NODE_ENV == 'production' ? 'animes-api.natanapps.repl.co/v1/' : 'https://animes.natanapps.repl.co/v1/'
