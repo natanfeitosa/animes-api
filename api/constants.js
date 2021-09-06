7d4d1797b2b34ada5f47e22c4d12f8f73877ee88
Natanael dos Santos Feitosa <52074821+natanfeitosa@users.noreply.github.com>
2021-09-05 21:29:35 -0300

    Update constants.js

--- api/constants.js
+++ api/constants.js
@@ -1,3 +1,3 @@
 exports.base_url = 'https://www.anitube.site/'
 
-exports.base_api = proccess.env.NODE_ENV == 'production' ? 'animes-api.natanapps.repl.co/v1/' : 'https://animes.natanapps.repl.co/v1/'
+exports.base_api = proccess.env.NODE_ENV == 'production' ? 'https://all-animes.herokuapp.com/v1/' : 'https://animes-api.natanapps.repl.co/v1/'
